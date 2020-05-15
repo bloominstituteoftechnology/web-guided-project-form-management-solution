bba70dc29c4ee7ed6af2ef639257b3930a8d0f5d
Gabriel Cabrejas <gabriel@cabrejas.net>
2020-05-15 15:48:39 +0200

    style a bit better

--- src/components/Friend.jsx
+++ src/components/Friend.jsx
@@ -8,7 +8,7 @@ function Friend({ details }) {
     <div className='friend container'>
       <h2>{details.username}</h2>
       <p>Email: {details.email}</p>
-      <p>Married: {details.married ? 'Yes' : 'No'}</p>
+      <p>Role: {details.role}</p>
 
       {
         !!details.hobbies && !!details.hobbies.length &&
--- src/components/FriendForm.jsx
+++ src/components/FriendForm.jsx
@@ -10,67 +10,74 @@ function FriendForm(props) {
   } = props
 
   return (
-    <form className='friend container'>
-      <h2>Friend Form</h2>
-      {/* ////////// TEXT INPUTS ////////// */}
-      <label>Username:&nbsp;
+    <form className='form container' onSubmit={onSubmit}>
+      <div className='form-group submit'>
+        <h2>Add a Friend</h2>
+        {/* 🔥 STEP 3C - THIS BUTTON WANTS A HANDLER! */}
+        <button>submit</button>
+      </div>
+      <div className='form-group inputs'>
+        {/* ////////// TEXT INPUTS ////////// */}
+        <h4>General information</h4>
+        <label>Username:&nbsp;
       <input
-          // 🔥 STEP 3A - THIS INPUT WANTS VALUE & CHANGE HANDLER!
-          // Inputs render what they're told
-          // Their current value ultimately comes from app state
-          // At each keystroke, a change handler should fire
-          value={values.username}
-          onChange={onInputChange}
-          name='username'
-          type='text'
-        /></label>
-      <label>Email:&nbsp;
+            // 🔥 STEP 3A - THIS INPUT WANTS VALUE & CHANGE HANDLER!
+            // Inputs render what they're told
+            // Their current value ultimately comes from app state
+            // At each keystroke, a change handler should fire
+            value={values.username}
+            onChange={onInputChange}
+            name='username'
+            type='text'
+          /></label>
+        <label>Email:&nbsp;
       <input
-          // 🔥 STEP 3B - THIS INPUT WANTS VALUE & CHANGE HANDLER!
-          value={values.email}
-          onChange={onInputChange}
-          name='email'
-          type='text'
-        /></label>
+            // 🔥 STEP 3B - THIS INPUT WANTS VALUE & CHANGE HANDLER!
+            value={values.email}
+            onChange={onInputChange}
+            name='email'
+            type='text'
+          /></label>
 
-      {/* ////////// DROPDOWN ////////// */}
-      <label>Lambda Role:&nbsp;
-      <select
-          // very similar to inputs of type text
-          onChange={onInputChange}
-          value={values.civil}
-          defaultValue=''
-          name='role'
-        >
-          <option value=''>Select an option</option>
-          <option value='Student'>Student</option>
-          <option value='Alumni'>Alumni</option>
-          <option value='TL'>Team Lead</option>
-          <option value='Instructor'>Instructor</option>
-        </select></label>
+        {/* ////////// DROPDOWN ////////// */}
+        <label>Role:&nbsp;
+        <select
+            // very similar to inputs of type text
+            onChange={onInputChange}
+            value={values.civil}
+            defaultValue=''
+            name='role'
+          >
+            <option value=''>- Select an option -</option>
+            <option value='Student'>Student</option>
+            <option value='Alumni'>Alumni</option>
+            <option value='TL'>Team Lead</option>
+            <option value='Instructor'>Instructor</option>
+          </select></label>
+      </div>
 
-      {/* ////////// CHECKBOXES ////////// */}
-      <label><input
-        // Checkboxes are different!
-        // We do NOT set their 'value'
-        // We use a 'checked' prop with a bool
-        checked={values.hobbies.hiking}
-        onChange={onCheckboxChange}
-        name='hiking'
-        type="checkbox" /> Hiking</label>
-      <label><input
-        checked={values.hobbies.reading}
-        onChange={onCheckboxChange}
-        name='reading'
-        type="checkbox" /> Reading</label>
-      <label><input
-        checked={values.hobbies.coding}
-        onChange={onCheckboxChange}
-        name='coding'
-        type="checkbox" /> Coding</label>
-
-      {/* 🔥 STEP 3C - THIS BUTTON WANTS A HANDLER! */}
-      <button onClick={onSubmit}>submit</button>
+      <div className='form-group checkboxes'>
+        {/* ////////// CHECKBOXES ////////// */}
+        <h4>Hobbies</h4>
+        <label>Hiking <input
+          // Checkboxes are different!
+          // We do NOT set their 'value'
+          // We use a 'checked' prop with a bool
+          checked={values.hobbies.hiking}
+          onChange={onCheckboxChange}
+          name='hiking'
+          type="checkbox" /></label>
+        <label>Reading <input
+          checked={values.hobbies.reading}
+          onChange={onCheckboxChange}
+          name='reading'
+          type="checkbox" /></label>
+        <label>Coding<input
+          checked={values.hobbies.coding}
+          onChange={onCheckboxChange}
+          name='coding'
+          type="checkbox" /></label>
+      </div>
     </form>
   )
 }
