960fb6f528290595c69a5304507de3fc0ebbe421
Gabriel Cabrejas <gabriel@cabrejas.net>
2020-05-18 22:57:43 +0200

    finish line

---
+++ lesson-plan.md
@@ -0,0 +1,5 @@
+# Lesson Plan
+
+Follow in order steps 1 to 9.
+You can use global search to find each `🔥 STEP`.
+All the work is done inside `App.jsx` and `FriendForm.jsx`
--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,44 +3,33 @@ import { v4 as uuid } from 'uuid'
 import Friend from './Friend'
 import FriendForm from './FriendForm'
 
+// 👉 the shape of the actual friend object from API
 const initialFriendsList = [
-  // 👉 the shape of the actual friend object from API
   {
     id: uuid(),
     username: 'Michael',
     email: 'michael@michael.com',
     role: 'Student',
-    hobbies: [
-      'hiking',
-      'reading',
-      'coding',
-    ]
   },
 ]
 
 // 👉 the shape of the state that drives the form
 const initialFormValues = {
-  ///// TEXT INPUTS (`value` is text-driven) /////
+  ///// TEXT INPUTS /////
   username: '',
   email: '',
-  ///// DROPDOWN (`value` is text-driven) /////
+  ///// DROPDOWN /////
   role: '',
-  ///// CHECKBOXES (`checked` is boolean-driven) /////
-  hobbies: {
-    hiking: false,
-    reading: false,
-    coding: false,
-  },
 }
 
 export default function App() {
   const [friends, setFriends] = useState(initialFriendsList)
 
   // 🔥 STEP 1 - WE NEED STATE TO HOLD ALL VALUES OF THE FORM!
-  const [formValues, setFormValues] = useState(initialFormValues)// fix this using the state hook
+  const [formValues, setFormValues] = useState(initialFormValues) // fix this using the state hook
 
   const onInputChange = evt => {
-    // 🔥 STEP 4 - IMPLEMENT A CHANGE HANDLER (works for inputs and dropdowns)
+    // 🔥 STEP 8 - IMPLEMENT A CHANGE HANDLER (works for inputs and dropdowns)
     // which can change the state of inputs of type text
 
     // a) pull the name of the input from the event object
@@ -56,27 +45,14 @@ export default function App() {
     })
   }
 
-  const onCheckboxChange = evt => {
-    // a) pull the name of the checkbox from the event
-    const { name } = evt.target
-    // b) pull whether checked true or false, from the event
-    const isChecked = evt.target.checked
-    // c) set a new state for the whole form
-    setFormValues({
-      ...formValues,
-      hobbies: {
-        ...formValues.hobbies,
-        [name]: isChecked,
-      }
-    })
-  }
-
   const onSubmit = evt => {
-    // 🔥 STEP 5 - IMPLEMENT A SUBMIT HANDLER
+    // 🔥 STEP 9 - IMPLEMENT A SUBMIT HANDLER
 
     // a) don't allow the browser to reload!
     evt.preventDefault()
-    // b) make a new friend object with an id, GROSS
+    // b) don't allow the submission, if any of the formValues is empty!
+    if (Object.values(formValues).some(input => !input.trim())) return
+    // c) make a new friend object
     //    set up the new friend with the correct attributes
     //    using the information inside the state of the form
     const newFriend = {
@@ -84,12 +60,10 @@ export default function App() {
       username: formValues.username,
       email: formValues.email,
       role: formValues.role,
-      hobbies: Object.keys(formValues.hobbies)
-        .filter(hobby => formValues.hobbies[hobby] === true)
     }
-    // c) update the list of friends in state with the new friend
-    setFriends([...friends, newFriend])
-    // d) optionally clear the form
+    // d) update the list of friends in state with the new friend
+    setFriends([newFriend, ...friends])
+    // e) optionally clear the form
     setFormValues(initialFormValues)
   }
 
@@ -98,12 +72,11 @@ export default function App() {
       <header><h1>Friends App</h1></header>
 
       <FriendForm
-        // 🔥 STEP 2 - THE FORM WANTS ITS FOOD!!!!
-        // check implementation of FriendForm
-        // to see what props it expects
+        // 🔥 STEP 2 - The form component needs its props.
+        //    Check implementation of FriendForm
+        //    to see what props it expects.
         values={formValues}
         onInputChange={onInputChange}
-        onCheckboxChange={onCheckboxChange}
         onSubmit={onSubmit}
       />
 
@@ -117,6 +90,3 @@ export default function App() {
     </div>
   )
 }
-
-// 🔥 STEP 6 - DO STEPS 3, 4 & 5 FOR THE DROPDOWN
-// 🔥 STEP 7 - DO STEPS 3, 4 & 5 FOR THE CHECKBOXES
