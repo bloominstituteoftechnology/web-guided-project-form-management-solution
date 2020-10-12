34f003b6667874722180acb60bca365056b80410
Rhiannon <11739072+rhiroy@users.noreply.github.com>
2020-10-12 18:57:00 -0500

    test

--- src/index.js
+++ src/index.js
@@ -1,39 +1,42 @@
 // 👉 Importing React libs from node_modules folder
-import React, { useState } from 'react'
-import { render } from 'react-dom'
+import React, { useState } from "react";
+import { render } from "react-dom";
 // 👉 App contains a more sophisticated form we'll flesh out later
-import App from './components/App'
+import App from "./components/App";
 
 // 👉 First let's build a SimpleForm to add more pets:
 const petsList = [
-  { petName: 'Fido', petType: 'dog' },
-  { petName: 'Tweetie', petType: 'canary' },
-  { petName: 'Goldie', petType: 'fish' },
-]
+  { petName: "Fido", petType: "dog" },
+  { petName: "Tweetie", petType: "canary" },
+  { petName: "Goldie", petType: "fish" },
+];
 
-const initialFormValues = { petName: '', petType: '' }
+const initialFormValues = { petName: "", petType: "" };
 
 function SimpleForm() {
-  // THE STATE
-  const [pets, setPets] = useState(petsList)
-  const [formValues, setFormValues] = useState(initialFormValues)
+  // THE STATE hello
+  const [pets, setPets] = useState(petsList);
+  const [formValues, setFormValues] = useState(initialFormValues);
   // THE EVENT HANDLERS THAT ALTER STATE
-  const change = evt => {
-    const { name, value } = evt.target
-    setFormValues({ ...formValues, [name]: value })
-  }
-  const submit = evt => {
+  const change = (evt) => {
+    const { name, value } = evt.target;
+    setFormValues({ ...formValues, [name]: value });
+  };
+  const submit = (evt) => {
     /*
     INSTRUCTOR: mention that without `preventDefault` the page would do a reload
     */
-    evt.preventDefault()
-    const newPet = { petName: formValues.petName.trim(), petType: formValues.petType.trim() }
-    setPets(pets.concat(newPet))
-    setFormValues(initialFormValues)
-  }
+    evt.preventDefault();
+    const newPet = {
+      petName: formValues.petName.trim(),
+      petType: formValues.petType.trim(),
+    };
+    setPets(pets.concat(newPet));
+    setFormValues(initialFormValues);
+  };
   // THE JSX
   return (
-    <div className='container'>
+    <div className="container">
       <h1>Simple Form App</h1>
       {
         /*
@@ -41,22 +44,35 @@ function SimpleForm() {
         We do it here because it makes the warning in the console go away for this quick demo.
         Resources from the backend will have unique ids and those should be the keys.
         */
-        pets.map((pet, idx) =>
-          <div key={idx}>{pet.petName} is a {pet.petType}</div>)
+        pets.map((pet, idx) => (
+          <div key={idx}>
+            {pet.petName} is a {pet.petType}
+          </div>
+        ))
       }
       <form onSubmit={submit}>
-        <input value={formValues.petName} onChange={change} name='petName' type="text" />
-        <input value={formValues.petType} onChange={change} name='petType' type="text" />
+        <input
+          value={formValues.petName}
+          onChange={change}
+          name="petName"
+          type="text"
+        />
+        <input
+          value={formValues.petType}
+          onChange={change}
+          name="petType"
+          type="text"
+        />
         <button>submit</button>
       </form>
     </div>
-  )
+  );
 }
 
 render(
   <>
     <SimpleForm />
     <App />
-  </>
-  , document.querySelector('#root')
-)
+  </>,
+  document.querySelector("#root")
+);
