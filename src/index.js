ef3f18a47783956a520f69c99583de765a4e9ffc
Casey Harding <casey.harding@generalassemb.ly>
2021-08-17 12:34:40 -0400

    Initial form in place.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { render } from 'react-dom'
 // 👉 App contains a more sophisticated form we'll flesh out later
 import App from './components/App'
@@ -11,7 +11,44 @@ const petsList = [
 ]
 
 function SimpleForm() {
-  return <div>Ready to start GP!</div>
+  const [formValues, setFormValues] = useState({petName: "", petType: ""});
+
+  const change = (event) => {
+    const { name, value } = event.target;
+
+    setFormValues({ ...formValues, [name]: value });
+  }
+
+  const submit = (event) => {
+    event.preventDefault();
+    const newPet = {
+      petName: formValues.petName,
+      petType: formValues.petType
+    }
+
+    console.log(newPet);
+  }
+
+  return (
+    <div className="container">
+      <h1>Simple Form App!</h1>
+      <form onSubmit={submit}>
+        <input
+          type="text"
+          name="petName"
+          value={formValues.petName}
+          onChange={change}
+        />
+        <input 
+          type="text"
+          name="petType"
+          value={formValues.petType}
+          onChange={change}
+        />
+        <input type="submit" value="CREATE YOUR PET" />
+      </form>
+    </div>
+  )
 }
 
 render(
